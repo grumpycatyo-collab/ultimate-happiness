d4e8ddc836614354770bf45a793707b1fbe98582
Plamadeala Max <linkolnmax24@gmail.com>
2024-03-27 00:39:11 +0200

    feat: add raw logging + vendoring

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,5 +1,47 @@
-package sales_api
+package main
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var build = "develop"
 
 func main() {
+
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": "SALES-API",
+	}
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/grumpycatyo-collab/ultimate-happiness
 
 go 1.21.8
 
-require github.com/ardanlabs/conf v1.5.0 // indirect
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.11.0 // indirect
