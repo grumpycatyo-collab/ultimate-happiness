1a54a2ff7108a13074830dbadf8eb3bc5b53bbc0
Plamadeala Max <linkolnmax24@gmail.com>
2024-03-30 15:06:54 +0200

    feat: add token generation in admin.go

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,15 +6,82 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	jwt "github.com/golang-jwt/jwt/v4"
+	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	err := genKey()
+	err := genToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func genToken() error {
+
+	file, err := os.Open("zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem")
+	if err != nil {
+		return err
+	}
+
+	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+
+	}
+
+	// =================================================================================================================
+
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "service project",
+			Subject:   "1924992432",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("======= TOKEN BEGIN =======")
+	fmt.Println(str)
+	fmt.Println("======= TOKEN END =======")
+	fmt.Print("\n")
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("error marshaling public key: %w", err)
+	}
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	return nil
 }
 
 func genKey() error {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.8
 require (
 	github.com/ardanlabs/conf v1.5.0
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.6.0
 	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.27.0
